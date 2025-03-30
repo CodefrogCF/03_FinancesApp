4f85092d98f3555ecb250fccf5d50552590a52ec
HagenK42 <666.thurisaz.666@gmail.com>
2025-03-31 00:06:10 +0200

    changed the html title

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ function isActiveForm($formName, $activeForm) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
+    <title>FrogFinances</title>
     <link rel="icon" href="./assets/favicon.ico">
     <link rel="stylesheet" href="styles.css">
 </head>
--- user_page.php
+++ user_page.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['email'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Page</title>
+    <title>FrogFinances</title>
     <link rel="icon" href="./assets/favicon.ico">
     <link rel="stylesheet" href="styles.css">
 </head>
