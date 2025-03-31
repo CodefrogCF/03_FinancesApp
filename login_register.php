d5d110807dc3b2d4fedf9d376e025dc298b416b0
HagenK42 <666.thurisaz.666@gmail.com>
2025-03-31 12:45:43 +0200

    changed the behaviour, when a db table for a new user gets created

--- login_register.php
+++ login_register.php
@@ -15,10 +15,10 @@ if (isset($_POST['register'])) {
         $_SESSION['active_form'] = 'register';
     } else {
         $task_db = "tasks_$name";
-        $conn->query("INSERT INTO users (name, email, password, role) VALUES ('$name', '$email', '$password', '$role')");
-        $conn->query("INSERT INTO users_tasks (user, task_db) VALUES ('$name', '$task_db')");
+        $conn->query("INSERT INTO users (name, email, password, role, user_db) VALUES ('$name', '$email', '$password', '$role', '$task_db')");
+        //$conn->query("INSERT INTO users_tasks (user, task_db) VALUES ('$name', '$task_db')");
         $conn->query("CREATE TABLE `usr_web33_2`.`$task_db` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `task_name` VARCHAR(255) NOT NULL , `task_value` FLOAT NOT NULL , `task_schedule` VARCHAR(255) NOT NULL , `task_isActive` VARCHAR(10) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;");
-        $conn->query("INSERT INTO tasks_$name (task_name, task_value, task_schedule, task_isActive) VALUES ('$name-Test-Task', '12.34', 'monthly', 'inactive')");
+        $conn->query("INSERT INTO $task_db (task_name, task_value, task_schedule, task_isActive) VALUES ('$name-Test-Task', '12.34', 'monthly', 'inactive')");
     }
 
     header("Location: index.php");
